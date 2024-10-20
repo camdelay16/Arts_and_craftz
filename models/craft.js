66a21bfe8d0d34622b381612b4d13a946e4ecc7f
camdelay16 <camdelay16@gmail.com>
2024-10-20 15:25:48 -0400

    updated craft schema

--- models/craft.js
+++ models/craft.js
@@ -4,14 +4,21 @@ const Craft = new Schema (
     {
         craftName: { type: String, required: true },
         craftType_id: { type: Schema.Types.ObjectId, ref: "Types"},
-        overview: { type: String, required: true},
-        difficulty: { type: String, required: true},
-        freeOrPremium: { type: Boolean, required: true},
+        tagline: { type: String, required: true },
+        difficulty: { type: String, required: true, enum: {values:['Easy','Medium', 'Hard', 'Expert']}},
+        premiumMembershio: { type: Boolean, required: true},
         craftImg: { type: String, required: true},
         forKids: { type: Boolean, required: true},
         description: { type: String, required: true},
-        materials: [{ type: String, required: true}],
-        directions: [{ type: String, required: true}],
+        materials: [{
+            amount: {type: Number},
+            unit: {type: Number},
+            item: { type: String},
+        }],
+        directions: [{ 
+            step: {type: Number, required: true},
+            direction: {type: String, required: true}
+        }],
         youtubeLink: { type: String, required: true}, 
         craftReviews: [{
             reviewer: { type: String, required: true },
